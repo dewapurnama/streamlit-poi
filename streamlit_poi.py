52a8a41dd6d44f92b12615c869dfb5c67568addd
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2023-06-16 10:01:23 +0700

    Update streamlit_poi.py

--- streamlit_poi.py
+++ streamlit_poi.py
@@ -227,7 +227,7 @@ if excel_file is not None:
     st.markdown("<h1 style='font-size: 20px;'>POI Distribution by Location</h1>", unsafe_allow_html=True)
     fig = px.scatter_mapbox(gdf_append, lat="Lat_POI", lon="Long_POI", hover_name="nama_POI", hover_data="POI", zoom=4)
     # Add the overlay circle
-    overlay = px.scatter_mapbox(gdf_append, lat="Lat_TBG", lon="Long_TBG", hover_name=gdf_append.columns.values[0], hover_data="Site_TBG", zoom=4)
+    overlay = px.scatter_mapbox(gdf_append, lat="Lat_TBG", lon="Long_TBG", hover_name=gdf_append.columns.values[0], zoom=4)
 
     fig.add_trace(overlay.data[0])
     fig.update_layout(mapbox_style="open-street-map")
